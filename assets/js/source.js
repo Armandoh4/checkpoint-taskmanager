b88ddc3fb17fa8e90b39a8bb0bef572be21bfba2
Armando Halilaj <armandohalilj4@hotmail.com>
2024-01-29 11:19:07 +0000

    Linked JS "add" Button for the first time

--- assets/js/source.js
+++ assets/js/source.js
@@ -1,12 +1,18 @@
-let addButton = id;
-addButton onclick{
-    if(addButton = 1){
-        display = on
-    }
-    else{addButton--,
-        Style = display:hidden;
-    }
-}
+function adInputBox() {
+    document.getElementById('addButton').style.display = "none";
+    
+};
 
+
+// let addButton = id;
+// addButton onclick{
+//     if(addButton = 1){
+//         display = on
+//     }
+//     else{addButton--,
+//         Style = display:hidden;
+//     }
+// }
+// for in loop to itterate through properties 
 
 // input from id=inputBox
